3e5686cf6064b8328b676195c6ebb97752def666
Luca Pattocchio <pattocchioluca@gmail.com>
2025-01-06 23:23:18 +0100

    feat: add progress bar provider using next-nprogress-bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import ProgressBarProvider from '@/components/progressbar-provider'
 import { Header } from '@/components/header'
 import { Hero } from '@/components/hero'
 import { Footer } from '@/components/footer'
@@ -26,14 +27,16 @@ export default async function RootLayout({
     return (
         <html lang="en">
             <body className={`${barlow.variable} px-0 antialiased sm:px-4 md:px-6 lg:px-8`}>
-                <div className="mx-auto my-0 w-full max-w-[1440px] overflow-hidden bg-white p-0">
-                    <Header />
-                    <Hero />
-                    <div className="px-6 pb-0 pt-6 sm:px-8 sm:pt-8 lg:px-16 lg:pt-16 xl:px-20 xl:pt-20">
-                        <main className="mx-auto mb-20 max-w-[880px]">{children}</main>
+                <ProgressBarProvider>
+                    <div className="mx-auto my-0 w-full max-w-[1440px] overflow-hidden bg-white p-0">
+                        <Header />
+                        <Hero />
+                        <div className="px-6 pb-0 pt-6 sm:px-8 sm:pt-8 lg:px-16 lg:pt-16 xl:px-20 xl:pt-20">
+                            <main className="mx-auto mb-20 max-w-[880px]">{children}</main>
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
+                </ProgressBarProvider>
             </body>
         </html>
     )
