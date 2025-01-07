433c2c0ce05c7ea084b40fd29a73e371b924509b
Luca Pattocchio <pattocchioluca@gmail.com>
2025-01-07 23:35:28 +0100

    fix: update paragraph styling to justify text in MDX components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,7 +2,9 @@ import type { MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
-        p: ({ children }: { children: React.ReactNode }) => <p className="text-base lg:text-lg xl:text-xl">{children}</p>,
+        p: ({ children }: { children: React.ReactNode }) => (
+            <p className="text-justify text-base lg:text-lg xl:text-xl">{children}</p>
+        ),
         h1: ({ children }: { children: React.ReactNode }) => (
             <h1 className="m-0 break-words p-0 pb-4 text-2xl font-bold text-[#CC4A49] sm:pb-6 sm:text-3xl lg:pb-7 lg:text-4xl xl:pb-8 xl:text-5xl">
                 {children}
