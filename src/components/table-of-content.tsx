2895122f42ecab5dc1e62cda58b964dea714f40a
Kasui92 <pattocchioluca@gmail.com>
2025-01-23 09:33:37 +0100

    fix: adjust toc font size;

--- src/components/table-of-content.tsx
+++ src/components/table-of-content.tsx
@@ -8,16 +8,18 @@ export type TableOfContentId = {
 
 export function TableOfContent({ ids }: { ids: TableOfContentId[] }) {
     return (
-        <div className="mb-8 space-x-2 text-justify text-[80%] text-[#CC4A49]">
-            <b>Indice.</b>
-            {ids.map(({ id, title }, i) => (
-                <Fragment key={i}>
-                    <Link href={`#${id}`} className="text-[#CC4A49] underline hover:no-underline">
-                        {title}
-                    </Link>
-                    {i < ids.length - 1 && <span> - </span>}
-                </Fragment>
-            ))}
+        <div className="text-[1.0625rem] leading-[1.75] sm:text-[1.0625rem] sm:leading-[1.75] lg:text-[1.125rem] lg:leading-[1.75] xl:text-[1.1875rem] xl:leading-[1.75]">
+            <div className="mb-8 space-x-2 text-justify text-[80%] text-[#CC4A49]">
+                <b>Indice.</b>
+                {ids.map(({ id, title }, i) => (
+                    <Fragment key={i}>
+                        <Link href={`#${id}`} className="text-[#CC4A49] underline hover:no-underline">
+                            {title}
+                        </Link>
+                        {i < ids.length - 1 && <span> - </span>}
+                    </Fragment>
+                ))}
+            </div>
         </div>
     )
 }
