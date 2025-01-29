34d589ec2cc34c85e25a885ca7ce285fbb0ac5b2
Luca Pattocchio <pattocchioluca@gmail.com>
2025-01-29 21:36:14 +0100

    feat: enhance header typography for larger screen sizes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { Navbar } from './navbar'
 function Brand(): ReactElement {
     return (
         <div className="m-0 my-auto flex max-w-full flex-col items-center p-0">
-            <p className="m-0 p-0 text-[1.5rem] font-bold leading-[1.4] sm:text-[1.75rem] sm:leading-[1.4]">
+            <p className="m-0 p-0 text-[1.5rem] font-bold leading-[1.4] sm:text-[1.75rem] sm:leading-[1.4] lg:text-[2rem] lg:leading-[1.4] xl:text-[2.25rem] xl:leading-[1.4]">
                 <Link href="/" className="transition-color duration-300 ease-in-out hover:text-[#CC4A49]">
                     Slice & Dice
                 </Link>
