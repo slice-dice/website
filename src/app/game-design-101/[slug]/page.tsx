9653dba9d19d0585e8d819f682cd1c1a94565eeb
Kasui92 <pattocchioluca@gmail.com>
2025-01-31 23:50:11 +0100

    feat: implement navigation for previous and next articles in Game Design 101

--- src/app/game-design-101/[slug]/page.tsx
+++ src/app/game-design-101/[slug]/page.tsx
@@ -1,13 +1,57 @@
+import { Main, MainFooter } from '@/components/main'
 import { gameDesign101 } from '@/data/game-design-101'
+import Link from 'next/link'
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const slug = (await params).slug
-    const { default: Docs } = await import(`../../../../docs/grafici-interattivi/${slug}.mdx`)
+    const { default: Docs } = await import(`../../../../docs/game-design-101/${slug}.mdx`)
+
+    // Get the position of the current slug
+    const currentSlug = gameDesign101.find(({ url }) => url === slug)
+    const currentIndex = gameDesign101.findIndex(({ url }) => url === slug) || 0
+
+    // Get the previous slug
+    const prevSlug = gameDesign101[currentIndex - 1]
+    // Get the next slug
+    const nextSlug = gameDesign101[currentIndex + 1]
 
     return (
-        <article className="prose lg:prose-xl">
-            <Docs />
-        </article>
+        <>
+            <Main>
+                <article className="prose lg:prose-xl">
+                    <h1 className="m-0 mb-4 break-words p-0 text-2xl font-bold text-[#CC4A49] sm:mb-6 sm:text-3xl lg:mb-7 lg:text-4xl xl:mb-8 xl:text-5xl">
+                        {currentSlug?.name}
+                    </h1>
+                    <Docs />
+                </article>
+            </Main>
+            <MainFooter>
+                <nav className="mx-auto my-0 w-full max-w-[1120px]">
+                    <div className="flex">
+                        {prevSlug && (
+                            <div className="m-0 w-1/2 break-words pr-4">
+                                <Link href={`/game-design-101/${prevSlug.url}`}>
+                                    <span className="block text-black/50">&laquo; Articolo precedente</span>
+                                    <h3 className="text-[1.25rem] font-bold leading-[1.4] text-[#CC4A49] no-underline hover:text-[#e36355] focus:outline-dotted active:text-[#e36355] lg:text-[1.375rem] lg:leading-[1.4] xl:text-[1.5rem] xl:leading-[1.4]">
+                                        {prevSlug.name}
+                                    </h3>
+                                </Link>
+                            </div>
+                        )}
+                        {nextSlug && (
+                            <div className="m-0 w-1/2 break-words pl-4 text-right">
+                                <Link href={`/game-design-101/${nextSlug.url}`}>
+                                    <span className="block text-black/50">Articolo successivo &raquo;</span>
+                                    <h3 className="text-[1.25rem] font-bold leading-[1.4] text-[#CC4A49] no-underline hover:text-[#e36355] focus:outline-dotted active:text-[#e36355] lg:text-[1.375rem] lg:leading-[1.4] xl:text-[1.5rem] xl:leading-[1.4]">
+                                        {nextSlug.name}
+                                    </h3>
+                                </Link>
+                            </div>
+                        )}
+                    </div>
+                </nav>
+            </MainFooter>
+        </>
     )
 }
 
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,11 @@
 export function Main({ children }: { children: React.ReactNode }) {
     return <main className="mx-auto mb-20 max-w-[880px]">{children}</main>
 }
+
+export function MainFooter({ children }: { children: React.ReactNode }) {
+    return (
+        <main className="-ml-6 -mr-6 border-t p-6 sm:-ml-12 sm:-mr-12 sm:p-12 lg:-ml-16 lg:-mr-16 lg:p-16 xl:-ml-20 xl:-mr-20 xl:p-20">
+            {children}
+        </main>
+    )
+}
