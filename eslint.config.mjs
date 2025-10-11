4fcfb6c7ba02ad8874d9c898fc876aef82777e6c
Luca Pattocchio <pattocchioluca@gmail.com>
2025-10-11 12:58:41 +0200

    Fixup eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import { fixupConfigRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-})
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
-const eslintConfig = [{
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')]
+const eslintConfig = [
+    ...fixupConfigRules(
+        compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+    ),
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts',
+        ],
+    },
+]
 
 export default eslintConfig
